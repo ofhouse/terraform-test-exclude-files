281715255382168b4057e6472385ae25ccf8879e
Felix Haus <felixhaus@gmail.com>
2021-03-07 18:15:21 +0100

    WIP

--- .github/actions/terraform-module-release/src/index.js
+++ .github/actions/terraform-module-release/src/index.js
@@ -8,6 +8,14 @@ async function run() {
   const releaseTag = core.getInput('release-tag', { required: true });
   const releaseBranchName = 'release';
 
+  // Push commits & tags on behalf of GitHub Actions bot
+  // https://github.com/actions/checkout/issues/13#issuecomment-724415212
+  await git.addConfig('user.name', 'github-actions[bot]');
+  await git.addConfig(
+    'user.email',
+    '41898282+github-actions[bot]@users.noreply.github.com'
+  );
+
   await git.checkout(releaseBranchName);
 
   core.info(`Creating new tag ${releaseTag}`);
