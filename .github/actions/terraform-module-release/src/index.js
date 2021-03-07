7f3272c7b7aa10e63e47c321cef6075c1b933bdd
Felix Haus <felixhaus@gmail.com>
2021-03-07 18:03:59 +0100

    WIP

--- .github/actions/terraform-module-release/src/index.js
+++ .github/actions/terraform-module-release/src/index.js
@@ -1,9 +1,23 @@
-const exec = require('@actions/exec');
+const core = require('@actions/core');
+const simpleGit = require('simple-git');
 
 async function run() {
+  const baseDir = path.join(process.cwd());
+  const git = simpleGit({ baseDir });
+  const releaseTag = core.getInput('release-tag', { required: true });
   const releaseBranchName = 'release';
 
-  await exec.exec('git', ['checkout', releaseBranchName]);
+  await git.checkout(releaseBranchName);
+
+  core.info(`Creating new tag ${releaseTag}`);
+  await git.addAnnotatedTag(releaseTag, `Release ${releaseTag}`);
+
+  // Push tag back to repo
+  git.pushTags('origin');
 }
 
-run();
+try {
+  run();
+} catch (error) {
+  console.log(error);
+}
